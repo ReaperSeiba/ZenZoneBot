b9cc37b22dfce148c1b7676b908aacb20c474597
Evie <yankeesareepic@gmail.com>
2024-05-15 19:13:47 +0000

    fix index js error

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const Discord = require("discord.js");
-const { Client } = require("discord.js");
+const { Client, GatewayIntentBits } = require("discord.js");
 const { registerSlashCommands } = require("./commands");
 const { handleInteraction } = require("./eventHandlers");
 
-const client = new Client();
-
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ],
+});
 const token = process.env["DISCORD_BOT_SECRET"];
 const channelId = "1220057482475339906";
 

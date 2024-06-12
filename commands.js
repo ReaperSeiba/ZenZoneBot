9ee243d0cb7a5156dcb2afa97336057d3a55c927
Evie <yankeesareepic@gmail.com>
2024-06-12 14:33:50 +0000

    fix error, add files for restructure, setup slash command functionality

--- commands.js
+++ commands.js
@@ -1,5 +1,29 @@
 async function registerSlashCommands(client) {
-  // Your registerSlashCommands function implementation
+  try {
+      await client.application.commands.set([
+          {
+              name: "store",
+              description: "Stores an item",
+              options: [
+                  {
+                      name: "name",
+                      description: "Name of the item",
+                      type: 3,
+                      required: true,
+                  },
+                  {
+                      name: "color",
+                      description: "Color of the item",
+                      type: 3,
+                      required: true,
+                  },
+              ],
+          },
+      ]);
+      console.log("Slash commands registered successfully!");
+  } catch (error) {
+      console.error("Error registering slash commands:", error);
+  }
 }
 
 module.exports = { registerSlashCommands };
